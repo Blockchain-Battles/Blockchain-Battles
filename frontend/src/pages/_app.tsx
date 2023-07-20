052606246db73411daa55de4da16d46f1d614cc0
Soroosh Jaberi <93154421+sorooshjaberi@users.noreply.github.com>
2023-07-21 00:14:52 +0430

    added parse config

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { initializeParse } from "@parse/react-ssr";
+import dotenv from "dotenv";
+dotenv.config();
+initializeParse(
+  process.env.INSERT_YOUR_CUSTOM_URL_HERE!, //custom url
+  process.env.INSERT_YOUR_APP_ID_HERE!, //app id
+  process.env.INSERT_YOUR_JS_KEY_HERE! //js
+);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
