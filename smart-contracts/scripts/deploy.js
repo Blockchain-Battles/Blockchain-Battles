16a1b8f9bb5ea73071037381eb1eeb0c1cced989
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2023-08-12 15:32:46 +0300

    add verify operation

--- smart-contracts/scripts/deploy.js
+++ smart-contracts/scripts/deploy.js
@@ -15,9 +15,25 @@ async function main() {
   await coinFlip.deployTransaction.wait(5)
   console.log(`CoinFlip deployed to ${coinFlip.address}`)
 
+  // Verifying CoinFlip
+  await verifyContract(coinFlip, 'CoinFlip')
 }
 
-
+async function verifyContract(token, contractName) {
+  try {
+    await hre.run('verify:verify', { address: token.address })
+    console.log(`\n\n --> ${contractName} verified on the scanner site`)
+  } catch (err) {
+    const already = err.message?.toLowerCase()?.includes('already verified')
+    if (already) {
+      console.log(`     Verification error: ${err.message}`)
+    } else {
+      // possible error reasons: API key is not set, network connection errors (we should be able to try again, to verify only)
+      console.log(`\n\n --> ${contractName} is not verified on the scanner site\n`)
+      throw err
+    }
+  }
+}
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
