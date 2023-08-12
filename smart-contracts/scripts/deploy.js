791043e353fdb938647d47f992c4968fed05b29e
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2023-08-12 15:32:14 +0300

    add deployment process notes

--- smart-contracts/scripts/deploy.js
+++ smart-contracts/scripts/deploy.js
@@ -4,16 +4,16 @@ const isPublicNetwork = !hre.network.name.includes('local')
 
 async function main() {
   // Deploy 
-
+  console.log('Deploying contracts...')
   const CoinFlip = await hre.ethers.getContractFactory('CoinFlip')
   const coinFlip = await CoinFlip.deploy()
 
   await coinFlip.deployed()
   
   // wait for 5 confirmations
-
+  console.log('coinFlip.deployed, waiting for 5 block confirmations...')
   await coinFlip.deployTransaction.wait(5)
-
+  console.log(`CoinFlip deployed to ${coinFlip.address}`)
 
 }
 
