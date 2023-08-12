d3a3a14fb77534f8d5c07a2e49d53eb2195e8c84
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2023-08-12 15:31:19 +0300

    fix flip coin deployment

--- smart-contracts/scripts/deploy.js
+++ smart-contracts/scripts/deploy.js
@@ -1,33 +1,27 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const hre = require('hardhat')
+
+
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  // Deploy 
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  const CoinFlip = await hre.ethers.getContractFactory('CoinFlip')
+  const coinFlip = await CoinFlip.deploy()
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  await coinFlip.deployed()
+  
+  // wait for 5 confirmations
 
-  await lock.waitForDeployment();
+  await coinFlip.deployTransaction.wait(5)
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+
 }
+
+
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  console.error(error)
+  process.exitCode = 1
+})
