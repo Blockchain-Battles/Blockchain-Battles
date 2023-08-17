eea3bd8a465316bb0cd79ed0ac2993a7e6ea3ea5
Soroosh Jaberi <93154421+sorooshjaberi@users.noreply.github.com>
2023-08-17 15:42:43 +0430

    added title to login page

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,18 +1,24 @@
 import LoginForm from "@/components/login/LoginForm";
+import Head from "next/head";
 import Image from "next/image";
 
 type Props = {};
 const Login = (props: Props) => {
   return (
-    <section className="mx-auto mt-8 flex h-[70vh] w-full max-w-[700px] items-center rounded-[7px] bg-[#eeeeeea2] shadow-md">
-      <div className="w-[50%] px-4 py-8 h-full">
-        <LoginForm />
-      </div>
+    <>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <section className="mx-auto mt-8 flex h-[70vh] w-full max-w-[700px] items-center rounded-[7px] bg-[#eeeeeea2] shadow-md">
+        <div className="h-full w-[50%] px-4 py-8">
+          <LoginForm />
+        </div>
 
-      <div className="flex h-full w-[50%] items-center justify-center bg-white">
-        <Image alt="login" src="/login.jpg" width={400} height={700} />
-      </div>
-    </section>
+        <div className="flex h-full w-[50%] items-center justify-center bg-white">
+          <Image alt="login" src="/login.jpg" width={400} height={700} />
+        </div>
+      </section>
+    </>
   );
 };
 export default Login;
