90f0ec9bd9f534b9c6ce40dd39e5b0718e2a4147
Soroosh Jaberi <93154421+sorooshjaberi@users.noreply.github.com>
2023-09-21 23:12:17 +0430

    feat(pages): dynamic page titles in games

--- frontend/src/pages/games/coinFlip.tsx
+++ frontend/src/pages/games/coinFlip.tsx
@@ -1,6 +1,14 @@
 import CoinFlipComponent from "@features/games/coinFlip/CoinFlip";
+import Head from "next/head";
 type Props = {};
 const CoinFlip = (props: Props) => {
-  return <CoinFlipComponent />;
+  return (
+    <>
+    <Head>
+      <title>Coin Flip</title>
+    </Head>
+      <CoinFlipComponent />
+    </>
+  );
 };
 export default CoinFlip;
