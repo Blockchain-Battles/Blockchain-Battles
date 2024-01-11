09d0c3cad1e3f0a55b2accc10a7b5fe399f0837a
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2024-01-11 16:01:05 +0300

    export config

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,19 @@ import { ParseServer } from 'parse-server';
 import path from 'path';
 const __dirname = path.resolve();
 import http from 'http';
+
+export const config = {
+  databaseURI:
+    process.env.DATABASE_URI,
+  appId: process.env.APP_ID ,
+  masterKey: process.env.MASTER_KEY , //Add your master key here. Keep it secret!
+  liveQuery: {
+    classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
+  },
 };
+// Client-keys like the javascript key or the .NET key are not necessary with parse-server
+// If you wish you require them, you can set them as options in the initialization above:
+// javascriptKey, restAPIKey, dotNetKey, clientKey
 
 const app = express();
 
