658f45e46e2550fd116f5a00f5ef7a75cfa51b3f
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2024-02-25 11:59:29 +0400

    define variable in contract

--- smart-contracts/contracts/RandomContract.sol
+++ smart-contracts/contracts/RandomContract.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24
+pragma solidity ^0.8.0;
 
 contract RandomContract {
+    address private owner;
+    uint256 private nonce;
 
+    constructor() {
+        owner = msg.sender;
+        nonce = 0;
+    }
+
+  
 }
