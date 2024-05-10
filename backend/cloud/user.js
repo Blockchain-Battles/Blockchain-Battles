c9446909b58eac604d85ba7df29a405e57dc8876
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2024-05-10 14:07:52 +0400

    add google login

--- backend/cloud/user.js
+++ backend/cloud/user.js
@@ -1,3 +1,7 @@
+
+const jwt = require('jsonwebtoken');
+const { checkExists, useMasterKey } = require('./utils');
+
 Parse.Cloud.beforeSave(Parse.User, function (request, response) {
     var user = request.object;
     if (user.isNew()) {
@@ -8,6 +12,33 @@ Parse.Cloud.beforeSave(Parse.User, function (request, response) {
     }
 });
 
-Parse.Cloud.afterSave(Parse.User, function (request, response) {
+Parse.Cloud.define("google-login", async (req) => {
+    const { params } = req;
+    const { id_token } = params;
+    checkExists(id_token, 'id_token')
 
-})
+    // Decode the token (without verification), if verification is needed it can be done by using the Google's API or with secret keys
+    const decoded = jwt.decode(id_token);
+    const { sub: id, email, picture, given_name, family_name } = decoded;
+    try {
+        const session = await Parse.User.logInWith('google', {
+            authData: {
+                id,  // googleId,
+                id_token
+            },
+        });
+
+        const user = await new Parse.Query(Parse.User).get(session.id, useMasterKey);
+        const avatar = new Parse.File('avatar', { uri: picture });
+        await avatar.save(useMasterKey)
+        user.setEmail(email);
+        user.set("firstname", given_name);
+        user.set("lastname", family_name);
+        user.set("avatar", avatar);
+        await user.save(null, useMasterKey);
+        return session;
+    } catch (e) {
+        console.log(e);
+        return e
+    }
+});
