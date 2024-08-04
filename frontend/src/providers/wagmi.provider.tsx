a74c47563d2d7725131c7ec7646e9fb4b62ef0f3
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2024-08-04 15:24:06 +0400

    change the using network to bsc mainnet

--- frontend/src/providers/wagmi.provider.tsx
+++ frontend/src/providers/wagmi.provider.tsx
@@ -1,13 +1,13 @@
 import { FC, PropsWithChildren } from "react";
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import config from "@/config";
-import {  localhost, sepolia } from "viem/chains";
+import { bsc} from "viem/chains";
 import { WagmiProvider as WProvider } from "wagmi";
 
 const rainbowConfig = getDefaultConfig({
   appName: "BlockChainBattles",
   projectId: config.projectId!,
-  chains: [sepolia, localhost],
+  chains: [bsc],
   ssr: true,
 });
 
