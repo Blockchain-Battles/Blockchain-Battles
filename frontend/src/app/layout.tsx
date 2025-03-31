07fab4231ffcfd8809408906ec868ea16454350e
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2025-03-31 20:47:07 +0400

    add google analytics

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import "@/assets/global.css";
 import { ReactNode } from "react";
 import clsx from "clsx";
 import Layout from "@/components/layouts/RootLayout";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function RootLayout(
   props: Readonly<{
@@ -14,6 +15,7 @@ export default function RootLayout(
     <html lang="en">
       <body className={clsx("relative h-dvh w-screen overflow-hidden")}>
         <Layout>{children}</Layout>
+        <GoogleAnalytics gaId="G-780DNEFLNM" />
       </body>
     </html>
   );
